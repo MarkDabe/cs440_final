3d88d09ac0f2eff5c4466f495b31d51153b10b7d
markhabashy <markhabashy91@gmail.com>
2019-04-20 01:01:34 -0400

    finished perceptron and naive bayes.

---
+++ naiveBayes.py
@@ -0,0 +1,160 @@
+import util
+import classificationMethod
+from collections import Counter
+from collections import defaultdict
+import math
+
+
+class NaiveBayesClassifier(classificationMethod.ClassificationMethod):
+    """
+    See the project description for the specifications of the Naive Bayes classifier.
+
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
+
+    def __init__(self, legalLabels):
+        self.legalLabels = legalLabels
+        self.type = "naivebayes"
+        self.k = 1  # this is the smoothing parameter, ** use it in your train method **
+        self.automaticTuning = False  # Look at this flag to decide whether to choose k automatically ** use this in your train method **
+
+    def setSmoothing(self, k):
+        """
+        This is used by the main method to change the smoothing parameter before training.
+        Do not modify this method.
+        """
+        self.k = k
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        """
+        Outside shell to call your method. Do not modify this method.
+        """
+
+        self.features = trainingData[0].keys()  # this could be useful for your code later...
+
+        if (self.automaticTuning):
+            kgrid = [0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
+        else:
+            kgrid = [self.k]
+
+        self.trainAndTune(trainingData, trainingLabels, validationData, validationLabels, kgrid)
+
+    def _occurrences(self, outcome):
+        no_of_examples = len(outcome)
+        prob = dict(Counter(outcome))
+        for key in prob.keys():
+            prob[key] = prob[key] / float(no_of_examples)
+        return prob
+
+    def trainAndTune(self, trainingData, trainingLabels, validationData, validationLabels, kgrid):
+
+        """
+        Trains the classifier by collecting counts over the training data, and
+        stores the Laplace smoothed estimates so that they can be used to classify.
+        Evaluate each value of k in kgrid to choose the smoothing parameter
+        that gives the best accuracy on the held-out validationData.
+
+        trainingData and validationData are lists of feature Counters.  The corresponding
+        label lists contain the correct label for each datum.
+
+        To get the list of all possible features or labels, use self.features and
+        self.legalLabels.
+        """
+        no_of_examples = len(trainingLabels)
+        prior_prob = dict(Counter(trainingLabels))
+        for key in prior_prob.keys():
+            prior_prob[key] = prior_prob[key] / float(no_of_examples)
+
+        likelihoods = dict()
+        for cls, prob in prior_prob.items():
+            # initializing the dictionary
+            likelihoods[cls] = defaultdict(list)
+
+        for cls, prob in prior_prob.items():
+            # taking samples of only 1 class at a time
+            row_indices = list()
+            for index, value in enumerate(trainingLabels):
+                if value == cls:
+                    row_indices.append(index)
+
+            subset = list()
+            for index in row_indices:
+                subset.append(trainingData[index])
+
+            for r in range(len(subset)):
+                for key, value in subset[r].items():
+                    likelihoods[cls][key].append(value)
+
+        classes = [key for key in prior_prob]
+        _like = likelihoods
+        for cls in classes:
+            for key, value in likelihoods[cls].items():
+                likelihoods[cls][key] = self._occurrences(likelihoods[cls][key])
+
+        results = {}
+        correct = 0
+        for itr in range(len(validationData)):
+            for cls in classes:
+                class_probability = prior_prob[cls]
+                for key, value in validationData[itr].items():
+                    relative_feature_values = likelihoods[cls][key]
+                    class_probability *= relative_feature_values.get(validationData[itr][key], 0.01)
+
+                results[cls] = class_probability
+
+            norm_factor = 0.0
+
+            for key, value in results.items():
+                norm_factor += value
+
+            for key in results:
+                try:
+                    results[key] = results[key]/norm_factor
+                except ZeroDivisionError:
+                    pass
+
+            if (list(results.keys())[list(results.values()).index(max([value for key, value in results.items()]))]) == validationLabels[itr]:
+                correct += 1
+
+        print "validation accuracy: {}%".format((correct/float(len(validationLabels))) * 100)
+
+
+    def classify(self, testData):
+        """
+        Classify the data based on the posterior distribution over labels.
+
+        You shouldn't modify this method.
+        """
+        guesses = []
+        self.posteriors = []  # Log posteriors are stored for later data analysis (autograder).
+        for datum in testData:
+            posterior = self.calculateLogJointProbabilities(datum)
+            guesses.append(posterior.argMax())
+            self.posteriors.append(posterior)
+        return guesses
+
+    def calculateLogJointProbabilities(self, datum):
+        """
+        Returns the log-joint distribution over legal labels and the datum.
+        Each log-probability should be stored in the log-joint counter, e.g.
+        logJoint[3] = <Estimate of log( P(Label = 3, datum) )>
+        """
+        logJoint = util.Counter()
+
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+        return logJoint
+
+    def findHighOddsFeatures(self, label1, label2):
+        """
+        Returns the 100 best features for the odds ratio:
+                P(feature=1 | label1)/P(feature=1 | label2)
+        """
+        featuresOdds = []
+
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+        return featuresOdds
---
+++ perceptron.py
@@ -0,0 +1,110 @@
+# Perceptron implementation
+import util
+import random
+
+PRINT = True
+
+
+class PerceptronClassifier:
+    """
+    Perceptron classifier.
+
+    Note that the variable 'datum' in this code refers to a counter of features
+    (not to a raw samples.Datum).
+    """
+
+    def __init__(self, legalLabels, max_iterations):
+        self.legalLabels = legalLabels
+        self.type = "perceptron"
+        self.max_iterations = max_iterations
+        self.weights = {}
+        for label in legalLabels:
+            self.weights[label] = util.Counter()  # this is the data-structure you should use
+
+    def _predict(self, data, label):
+        activation = 0.0
+        for key, value in self.weights[label].items():
+            activation += value * data[key]
+
+        return activation
+
+    def train(self, trainingData, trainingLabels, validationData, validationLabels):
+        """
+        The training loop for the perceptron passes through the training data several
+        times and updates the weight vector for each label based on classification errors.
+        See the project description for details.
+
+        Use the provided self.weights[label] data structure so that
+        the classify method works correctly. Also, recall that a
+        datum is a counter from features to values for those features
+        (and thus represents a vector a values).
+        """
+
+        self.features = trainingData[0].keys()  # could be useful later
+
+        for key in self.weights:
+            for feature in self.features:
+                self.weights[key][feature] = random.uniform(0.0, 1.0)
+
+        for iteration in range(self.max_iterations):
+            print("Starting iteration ", iteration, "...")
+            for itr in range(len(trainingData)):
+                activations = dict()
+                for key_1, value_1 in self.weights.items():
+                    activation = 0.0
+                    for key_2, value_2 in value_1.items():
+                        activation += value_2 * trainingData[itr][key_2]
+                    activations[key_1] = activation
+
+                max_activation = 0
+
+                for key, value in activations.items():
+                    if max_activation <= value:
+                        max_activation = value
+
+                if max_activation == trainingLabels[itr]:
+                    continue
+                else:
+                    for key, value in trainingData[itr].items():
+                        self.weights[trainingLabels[itr]][key] = self.weights[trainingLabels[itr]][key] + value
+
+                    other_classes = [key for key in self.weights]
+
+                    other_classes.remove(trainingLabels[itr])
+
+                    for cls in other_classes:
+                        for key, value in trainingData[itr].items():
+                            self.weights[cls][key] = self.weights[cls][key] - value
+
+                # prediction = self._predict(trainingData[itr], trainingLabels[itr])
+                # error = trainingLabels[itr] - prediction
+                # for key, value in trainingData[itr].items():
+                #     self.weights[trainingLabels[itr]][key] = self.weights[trainingLabels[itr]][key] + 0.1 * error * value
+
+    def classify(self, data):
+        """
+        Classifies each datum as the label that most closely matches the prototype vector
+        for that label.  See the project description for details.
+
+        Recall that a datum is a util.counter...
+        """
+        guesses = []
+        for datum in data:
+            vectors = util.Counter()
+            for l in self.legalLabels:
+                vectors[l] = self.weights[l] * datum
+            guesses.append(vectors.argMax())
+        return guesses
+
+    def findHighOddsFeatures(self, label1, label2):
+        """
+        Returns a list of the 100 features with the greatest difference in weights:
+                         w_label1 - w_label2
+
+        """
+        featuresOdds = []
+
+        "*** YOUR CODE HERE ***"
+        util.raiseNotDefined()
+
+        return featuresOdds
